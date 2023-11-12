48093790fea427c4751a30b3e61a5bc82af93881
shkisme <shkisme0130@gmail.com>
2023-11-12 11:54:37 +0900

    feat: 날짜 입력 기능과 유효하지 않은 날짜 예외처리 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,8 +5,31 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
-        return Integer.parseInt(input);
+        while (true) {
+            try {
+                String input = Console.readLine();
+                validateNumber(input);
+                validateDate(input);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void validateNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateDate(String input) {
+        int date = Integer.parseInt(input);
+        if (date < 1 || 31 < date) {
+            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
     }
 
     public String readMenu() {
