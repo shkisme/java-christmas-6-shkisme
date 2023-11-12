1b48f0819f26f75c1058fefbf020e23376220ccd
shkisme <shkisme0130@gmail.com>
2023-11-12 11:54:59 +0900

    feat: Application code 작성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,20 @@
 package christmas;
 
+import christmas.controller.Controller;
+import christmas.dao.menu.MenuEnumRepository;
+import christmas.dao.menu.MenuRepository;
+import christmas.model.benefits.ChristmasBenefits;
+import christmas.model.menu.Menu;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MenuRepository menuRepository = new MenuEnumRepository();
+        Menu presentation = menuRepository.findByName("샴페인");
+        Controller controller = new Controller(new InputView(),
+                new OutputView(),
+                new ChristmasBenefits(presentation));
+        controller.run();
     }
 }
