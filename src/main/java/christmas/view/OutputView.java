af79c635f2099db8b7334924566a801b818d58af
shkisme <shkisme0130@gmail.com>
2023-11-13 14:26:57 +0900

    feat: 이벤트 주의 사항 출력 기능 추가

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -18,6 +18,10 @@ public class OutputView {
 
     public void printStartMessage() {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("<예약 규칙>");
+        System.out.println("총주문 금액 10,000원 이상부터 이벤트가 적용됩니다.");
+        System.out.println("음료만 주문할 수 없습니다.");
+        System.out.println("메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다.\n");
     }
 
     public void printResultMessage() {
