7231e70d9d87d04504c3e2c6682b5addf51e7b9c
shkisme <shkisme0130@gmail.com>
2023-11-14 12:07:16 +0900

    feat: 주문과 주문 리스트 객체 생성 시 예외처리 추가

--- src/main/java/christmas/model/order/Order.java
+++ src/main/java/christmas/model/order/Order.java
@@ -1,14 +1,27 @@
 package christmas.model.order;
 
+import static christmas.exception.InvalidOrderException.InvalidOrderError.INVALID_COUNT;
+import static christmas.model.order.Orders.MAX_COUNT;
+import static christmas.model.order.Orders.MIN_COUNT;
+
+import christmas.exception.InvalidOrderException;
 import christmas.model.menu.Menu;
+import java.util.Objects;
 
 public class Order {
     private final Menu menu;
     private final int count;
 
     public Order(Menu menu, int count) {
+        validateCount(count);
         this.menu = menu;
         this.count = count;
+    }
+
+    private void validateCount(int count) {
+        if (count < MIN_COUNT || MAX_COUNT < count) {
+            throw new InvalidOrderException(INVALID_COUNT);
+        }
     }
 
     public int getPrice() {
@@ -25,5 +38,22 @@ public class Order {
 
     public int getCount() {
         return count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Order order = (Order) o;
+        return Objects.equals(menu, order.menu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menu);
     }
 }
--- src/main/java/christmas/model/order/Orders.java
+++ src/main/java/christmas/model/order/Orders.java
@@ -1,12 +1,18 @@
 package christmas.model.order;
 
-import static christmas.exception.InvalidOrderException.InvalidOrderError.ORDER_RESTRICTIONS;
+import static christmas.exception.InvalidOrderException.InvalidOrderError.DUPLICATE;
+import static christmas.exception.InvalidOrderException.InvalidOrderError.INVALID_COUNT;
+import static christmas.exception.InvalidOrderException.InvalidOrderError.INVALID_TYPE;
 import static java.util.Collections.unmodifiableList;
 
 import christmas.exception.InvalidOrderException;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Orders {
+    static final int MIN_COUNT = 1;
+    static final int MAX_COUNT = 20;
     private static final String RESTRICTION_MENU_NAME = "음료";
     private static final int BENEFITS_PRICE = 10_000;
     private static final int PRESENTS_PRICE = 120_000;
@@ -14,15 +20,38 @@ public class Orders {
     private final List<Order> orders;
 
     public Orders(List<Order> orders) {
-        validateOrders(orders);
+        validateType(orders);
+        validateDuplicate(orders);
+        validateCount(orders);
         this.orders = orders;
     }
 
-    private void validateOrders(List<Order> orders) {
+    private void validateType(List<Order> orders) {
         boolean isAllDrink = orders.stream().allMatch(order -> order.isType(RESTRICTION_MENU_NAME));
         if (isAllDrink) {
-            throw new InvalidOrderException(ORDER_RESTRICTIONS);
+            throw new InvalidOrderException(INVALID_TYPE);
         }
+    }
+
+    private void validateDuplicate(List<Order> orders) {
+        Set<Order> nonDuplicateOrder = new HashSet<>(orders);
+        if (nonDuplicateOrder.size() != orders.size()) {
+            throw new InvalidOrderException(DUPLICATE);
+        }
+    }
+
+    private void validateCount(List<Order> orders) {
+        int menuCountSum = getMenuCountSum(orders);
+        if (menuCountSum < MIN_COUNT || MAX_COUNT < menuCountSum) {
+            throw new InvalidOrderException(INVALID_COUNT);
+        }
+    }
+
+    private int getMenuCountSum(List<Order> orders) {
+        return orders.stream()
+                .map(Order::getCount)
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
     public int getPrice() {
